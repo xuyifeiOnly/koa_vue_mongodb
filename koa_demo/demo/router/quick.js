73a003def09ad532e7d08a29a4ea92cba3779489
xyf <1227161950@qq.com>
2019-07-07 14:32:48 +0800

    测试提交

--- koa_demo/demo/router/quick.js
+++ koa_demo/demo/router/quick.js
@@ -7,7 +7,7 @@ const Router = require('koa-router')
 let home = new Router()
 
 // 子路由1
-home.get('/', async ( ctx )=>{
+home.get('/', async (ctx) => {
   let html = `
     <ul>
       <li><a href="/page/helloworld">/page/helloworld</a></li>
@@ -19,9 +19,9 @@ home.get('/', async ( ctx )=>{
 
 // 子路由2
 let page = new Router()
-page.get('/404', async ( ctx )=>{
+page.get('/404', async (ctx) => {
   ctx.body = '404 page!'
-}).get('/helloworld', async ( ctx )=>{
+}).get('/helloworld', async (ctx) => {
   ctx.body = 'helloworld page!'
 })
 
@@ -31,9 +31,10 @@ router.use('/', home.routes(), home.allowedMethods())
 router.use('/page', page.routes(), page.allowedMethods())
 
 // 加载路由中间件
-app.use(router.routes()).use(router.allowedMethods())
+app
+  .use(router.routes())
+  .use(router.allowedMethods())
 
 app.listen(3000, () => {
   console.log('[demo] route-use-middleware is starting at port 3000')
 })
-
