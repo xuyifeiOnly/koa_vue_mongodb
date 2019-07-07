726a885be2afd1e0391c2b62c64b32efb473eb8d
xyf <1227161950@qq.com>
2019-07-07 09:34:05 +0800

    添加读取文件

--- koa_demo/demo/stream/get.js
+++ koa_demo/demo/stream/get.js
@@ -6,21 +6,33 @@ const app = new Koa()
 
 function getData(ctx) {
   let res = ctx.res
+  let result = {
+    success: true,
+    message: '',
+    data: null
+  }
   return new Promise((resolve, reject) => {
     var fileName = path.resolve(__dirname, 'get.txt');
     var stream = fs.createReadStream(fileName);
-    stream.pipe(res);
+    let chun = ''
+    stream.on('data', (chunk) => {
+      chun+=chunk.toString()
+    })
+    stream.on('end', function() {
+      result.data = JSON.parse(chun)
+      resolve(result)
+    })
+    // stream.pipe(res);
     // 将一个可读流通过 pipe 输送给res
   })
-    
+
 }
 
 app.use(async (ctx) => {
   let method = ctx.method
   if (method === 'GET') {
-    await getData(ctx)
-    // ctx.body = data
-
+    const result = await getData(ctx)
+    ctx.body = result
   }
 })
 
